43b2cdf26011b526ab282fb6907544ff83863b01
genaton <genaton@gmail>
2026-04-22 20:38:30 -0300

    Novo problema detectado na classe AdocaoService. Apesar de funcional, o código ainda apresenta muitas condições/validações misturadas. Aqui empregamos o S (princípio da responsabilidade única) do SOLID criando um pacote Validacoes e criando uma classe única para cada validacao e implementamos o padrão Strategy.

---
+++ src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivel.java
@@ -0,0 +1,26 @@
+package br.com.alura.adopet.api.validacoes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDTO;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+
+@Component
+public class ValidacaoPetDisponivel {
+    @Autowired
+    private PetRepository petRepository;
+
+    public void validar(SolicitacaoAdocaoDTO dto){
+
+        Pet pet = petRepository.getReferenceById(dto.idPet());
+
+        if (pet.getAdotado()) {
+            throw new ValidacaoException("Pet já foi adotado!");
+
+        }
+    }
+
+}
