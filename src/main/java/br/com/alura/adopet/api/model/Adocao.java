509f7f74247ee7eb5e47419551aefab0704eece4
genaton <genaton@gmail>
2026-04-22 22:31:02 -0300

    Refactoring do model. Observamos que a classe Adocao tem algumas configurações desnecessárias. Poderia ter seguido a convenção do JPA para entidades (COC - Convention over Configuration). Foram apagadas as anotacoes que eram redundantes e as anotacoes @Jason pois não usamos mais a entidade diretamente no controller.

--- src/main/java/br/com/alura/adopet/api/model/Adocao.java
+++ src/main/java/br/com/alura/adopet/api/model/Adocao.java
@@ -10,41 +10,36 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
-@Table(name = "adocoes")
+@Table(name = "adocoes") // configurado pois a entidade não segue a convenção que é o nome da classe com
+                         // a primeira letra em maiusculo.
 public class Adocao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+
     private Long id;
 
-    @Column(name = "data")
     private LocalDateTime data;
 
     @ManyToOne
-    @JsonBackReference("tutor_adocoes")
-    @JoinColumn(name = "tutor_id")
     private Tutor tutor;
 
     @OneToOne
-    @JoinColumn(name = "pet_id")
-    @JsonManagedReference("adocao_pets")
     private Pet pet;
 
-    @Column(name = "motivo")
     private String motivo;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status")
     private StatusAdocao status;
 
-    @Column(name = "justificativa_status")
     private String justificativaStatus;
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Adocao adocao = (Adocao) o;
         return Objects.equals(id, adocao.id);
     }
